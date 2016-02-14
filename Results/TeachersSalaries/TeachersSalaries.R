d4b35dd995546e7609ff0630470b307b94788579
Simon Lebastard <lebastardsimon@gmail.com>
2016-02-14 20:44:36 +0100

    Commit S.Lebastard #1 - TeachersSalaries.R was completed with Fisher tests for maths and sciences PISA results

--- Results/TeachersSalaries/TeachersSalaries.R
+++ Results/TeachersSalaries/TeachersSalaries.R
@@ -39,3 +39,28 @@ Slr2012PISAS <- na.omit(Slr2012PISAS)					# 33 entries
 colnames(Slr2012PISAS) <- c("CTR", "Teachers Salaries USD", "PISA Sciences Score")
 plot(Slr2012PISAS$"Teachers Salaries USD", Slr2012PISAS$"PISA Sciences Score")	# That also looks promising
 
+# Creating discrete dataframes #
+
+Discrete_Slr_M10 <- discretize2d(Slr2012PISAM$"Teachers Salaries USD", Slr2012PISAM$"PISA Maths Score", numBins1=10, numBins2=10)
+Discrete_Slr_S10 <- discretize2d(Slr2012PISAS$"Teachers Salaries USD", Slr2012PISAS$"PISA Sciences Score", numBins1=10, numBins2=10)
+
+# Fisher Testing #
+
+SlrFisherM10 <- fisher.test(Discrete_Slr_M10)
+	# Fisher's Exact Test for Count Data
+	# data:  Discrete_Slr_M10
+	# p-value = 0.02672
+	# alternative hypothesis: two.sided
+SlrFisherS10 <- fisher.test(Discrete_Slr_S10)
+	#
+	# Error in fisher.test(Discrete_Slr_S10): FEXACT error 30
+	# Stack length exceeded in f3xact
+	#
+
+Slr_miM10 <- mi.plugin(Discrete_Slr_M10)
+Slr_entrM10 <- entropy(Discrete_Slr_M10)
+SlrDependencyM10 <- (Slr_miM10)/(Slr_entrM10)
+
+Slr_miS10 <- mi.plugin(Discrete_Slr_S10)
+Slr_entrS10 <- entropy(Discrete_Slr_S10)
+SlrDependencyS10 <- (Slr_miS10)/(Slr_entrS10)
